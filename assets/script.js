b15810f8fb52e05c7f2fc93c5b69bca7c9f2e22c
igor <diegoigor2001@gmail.com>
2022-08-10 21:36:51 -0300

    finalizando aula 02

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
-document
-    .querySelector("button.fat")
-    .addEventListener("click", function(){
-        document
-            .querySelector("#footer")
-            .classList
-            .toggle("hide")
-    })
+function onOff(){
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("hide")
+}
+
+
